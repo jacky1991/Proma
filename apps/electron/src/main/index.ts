ae8dfa42e8a417c77e1b0533aa73ad6d5bd9d462
ErlichLiu <erlichgutu@gmail.com>
2026-04-30 20:44:34 +0800

    fix: followWindow

--- apps/electron/src/main/index.ts
+++ apps/electron/src/main/index.ts
@@ -178,7 +178,7 @@ function createWindow(): void {
     titleBarStyle: 'hiddenInset', // macOS style
     trafficLightPosition: { x: 18, y: 18 },
     vibrancy: 'under-window', // macOS glass effect
-    visualEffectState: 'followsWindowActiveState',
+    visualEffectState: 'followWindow',
   })
 
   // Load the renderer
